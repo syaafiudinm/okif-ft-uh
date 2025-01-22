c400a66467b3415e2ab463aefacff9b9e871d30c
syaafiudinm <altafpasallo12@gmail.com>
2025-01-22 22:52:18 +0800

    ref: initialize supabase as backend service

--- src/app/Tentang/page.js
+++ src/app/Tentang/page.js
@@ -1,13 +1,51 @@
-import React from 'react'
-import Navbar from '@/components/Navbar'
+"use client";
+
+import React, { useEffect, useState } from "react";
+import Navbar from "@/components/Navbar";
+import { supabase } from "@/lib/supabaseClient";
 
 export default function Tentang() {
-    return (
-        <>
-        <Navbar/>
-            <div>
-                Ini Halaman Tentang
-            </div>
-        </>
-    )
+  const [loading, setLoading] = useState(true);
+  const [users, setUsers] = useState();
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      setLoading(true);
+      const { data, error } = await supabase.from("users").select("*");
+
+      if (error) {
+        setError(error.message);
+      } else {
+        setUsers(data);
+      }
+
+      setLoading(false);
+    };
+
+    fetchUsers();
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+
+  return (
+    <>
+      <Navbar />
+      <div className="mt-32">Ini Halaman Tentang</div>
+      <div>
+        <h1 className="text-2xl font-bold mb-4">Daftar Users</h1>
+        <ul className="space-y-2">
+          {users.map((user) => (
+            <li
+              key={user.id}
+              className="p-2 bg-gray-100 rounded shadow-sm hover:bg-gray-200"
+            >
+              {user.name} - {user.email}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 }
