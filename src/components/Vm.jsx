f12cfede56e4a59347cc45c97ebed45adb717b24
syaafiudinm <altafpasallo12@gmail.com>
2025-02-07 23:12:01 +0800

    feat = add visi misi card example

--- src/components/Vm.jsx
+++ src/components/Vm.jsx
@@ -1,87 +1,76 @@
+"use client";
 import React from "react";
+import { useState, useEffect } from "react";
+import { supabase } from "@/lib/supabaseClient";
+import Program from "@/app/program/page";
 
 const Vm = () => {
+  const [ketua, setKetua] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchKetua = async () => {
+      setLoading(true);
+      const { data, error } = await supabase.from("ketua_lembaga").select("*");
+      if (error) {
+        setError(error.message);
+      } else {
+        setKetua(data);
+      }
+      setLoading(false);
+    };
+    fetchKetua();
+  }, []);
+
+  if (loading)
+    return (
+      <div role="status">
+        <svg
+          aria-hidden="true"
+          className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+          viewBox="0 0 100 101"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+            fill="currentColor"
+          />
+          <path
+            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+            fill="currentFill"
+          />
+        </svg>
+        <span className="sr-only">Loading...</span>
+      </div>
+    );
+  if (error) return <div>Error: {error}</div>;
   return (
     <>
-      <div className="flex max-sm:flex-wrap mt-20 max-sm:gap-5">
-        <section className="bg-white dark:bg-gray-900 border-2 border-slate-500">
-          <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 bg-slate-100 rounded-lg">
-            <h3 className="mb-4 text-3xl font-extrabold tracking-tight leading-none text-gray-900 md:text-2xl lg:text-xl dark:text-white mt-10">
-              VISI & MISI HMIF FT-UH Periode 2024/2025
-            </h3>
-            <h1 className="mb-4 text-3xl font-extrabold tracking-tight leading-none text-gray-900 md:text-2xl lg:text-xl dark:text-white">
-              Menjadikan OKIF FT-UH sebagai organisasi yang menjunjung tinggi
-              profesionalisme dalam berlembaga, berintegitas, adil, juga sebagai
-              sarana peningkatan potensi kader demi tercapainya tujuan OKIF
-              FT-UH
-            </h1>
-            <ol>
-              <li>
-                <p className="mb-8 text-lg font-normal text-gray-700 lg:text-md sm:px-16 lg:px-48 dark:text-gray-400">
-                  1. Meningkatkan dan mengoptimalkan komunikasi internal maupun
-                  eksternal OKIF FT-UH
+      <div className="flex flex-wrap w-full justify-center mt-28">
+        {ketua.map((ketua) => (
+          <div key={ketua.id}>
+            <a
+              href="#"
+              className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow-sm md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+            >
+              <img
+                className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
+                src="/docs/images/blog/image-4.jpg"
+                alt=""
+              />
+              <div className="flex flex-col justify-between p-4 leading-normal">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  {ketua.lembaga}
+                </h5>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                  {ketua.visi}
                 </p>
-              </li>
-              <li>
-                <p className="mb-8 text-lg font-normal text-gray-700 lg:text-md sm:px-16 lg:px-48 dark:text-gray-400">
-                  2. Menciptakan suasana harmonis untuk organisasi agar anggota
-                  OKIF FT-UH maksimal dalam menjalankan tugas
-                </p>
-              </li>
-              <li>
-                <p className="mb-8 text-lg font-normal text-gray-700 lg:text-md sm:px-16 lg:px-48 dark:text-gray-400">
-                  3. Memaksimalkan pelaksanaan kegiatan keinformatikaan dan
-                  kegiatan keagamaan dalam softskill dan hardskill kader
-                </p>
-              </li>
-              <li>
-                <p className="mb-8 text-lg font-normal text-gray-700 lg:text-md sm:px-16 lg:px-48 dark:text-gray-400">
-                  4. Meningkatkan dan memaksimalkan partisipasi kader dalam
-                  pelaksanaan kegiatan OKIF FT-UH
-                </p>
-              </li>
-            </ol>
+              </div>
+            </a>
           </div>
-        </section>
-        <section className="bg-white dark:bg-gray-900 border-2 border-slate-500">
-          <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 bg-slate-100 rounded-lg">
-            <h3 className="mb-4 text-3xl font-extrabold tracking-tight leading-none text-gray-900 md:text-2xl lg:text-xl dark:text-white mt-10">
-              VISI & MISI HMIF FT-UH Periode 2024/2025
-            </h3>
-            <h1 className="mb-4 text-3xl font-extrabold tracking-tight leading-none text-gray-900 md:text-2xl lg:text-xl dark:text-white">
-              Menjadikan OKIF FT-UH sebagai organisasi yang menjunjung tinggi
-              profesionalisme dalam berlembaga, berintegitas, adil, juga sebagai
-              sarana peningkatan potensi kader demi tercapainya tujuan OKIF
-              FT-UH
-            </h1>
-            <ol>
-              <li>
-                <p className="mb-8 text-lg font-normal text-gray-700 lg:text-md sm:px-16 lg:px-48 dark:text-gray-400">
-                  1. Meningkatkan dan mengoptimalkan komunikasi internal maupun
-                  eksternal OKIF FT-UH
-                </p>
-              </li>
-              <li>
-                <p className="mb-8 text-lg font-normal text-gray-700 lg:text-md sm:px-16 lg:px-48 dark:text-gray-400">
-                  2. Menciptakan suasana harmonis untuk organisasi agar anggota
-                  OKIF FT-UH maksimal dalam menjalankan tugas
-                </p>
-              </li>
-              <li>
-                <p className="mb-8 text-lg font-normal text-gray-700 lg:text-md sm:px-16 lg:px-48 dark:text-gray-400">
-                  3. Memaksimalkan pelaksanaan kegiatan keinformatikaan dan
-                  kegiatan keagamaan dalam softskill dan hardskill kader
-                </p>
-              </li>
-              <li>
-                <p className="mb-8 text-lg font-normal text-gray-700 lg:text-md sm:px-16 lg:px-48 dark:text-gray-400">
-                  4. Meningkatkan dan memaksimalkan partisipasi kader dalam
-                  pelaksanaan kegiatan OKIF FT-UH
-                </p>
-              </li>
-            </ol>
-          </div>
-        </section>
+        ))}
       </div>
     </>
   );
