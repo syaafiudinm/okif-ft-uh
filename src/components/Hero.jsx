e6b2f4dd80143e3b664e35702a30fad9f3fb54ee
fajri-fard <fajrijry14@gmail.com>
2025-02-13 20:36:25 +0800

    chore/beranda: tambahkan link di button jajaran pengurus okif

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -33,7 +33,7 @@ const Hero = () => {
               </svg>
             </a>
             <a
-              href="#"
+              href="/struktur"
               className="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 sm:ms-4 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400"
             >
               Jajaran Pengurus OKIF FT-UH
