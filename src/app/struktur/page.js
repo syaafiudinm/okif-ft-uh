33aae6fff0a7cc7e9d6a7e194442ee54be8af81e
syaafiudinm <altafpasallo12@gmail.com>
2025-02-15 01:49:28 +0800

    chore/struktur : rounded image + margin bottom

--- src/app/struktur/page.js
+++ src/app/struktur/page.js
@@ -10,7 +10,7 @@ export default function Tentang() {
       <Navbar />
       <div className="mt-32 px-4 md:px-10 lg:px-32 min-h-[50vh] lg:min-h-screen">
         <h1 className="font-bold text-lg md:text-2xl lg:text-4xl mb-4 md:mb-10 text-center">
-          Struktur Organisasi OKIF FT-UH Periode 2024/2025
+          Struktur Kepengurusan OKIF FT-UH Periode 2024/2025
         </h1>
         <Image
           src="/struktur.jpg"
@@ -19,6 +19,7 @@ export default function Tentang() {
           width={2000}
           height={500}
           style={{ width: "100%", height: "auto" }}
+          className="rounded-lg mb-14"
         ></Image>
       </div>
       <Footer />
