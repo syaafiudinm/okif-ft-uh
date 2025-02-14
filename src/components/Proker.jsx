b120459e2388c253455c6bea95bfc8299894e97b
syaafiudinm <altafpasallo12@gmail.com>
2025-02-15 01:37:37 +0800

    inital commit for merging

--- src/components/Proker.jsx
+++ src/components/Proker.jsx
@@ -3,6 +3,8 @@ import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
 import { supabase } from "../lib/supabaseClient";
+import Modal from "./Modal";
+
 
 const Proker = () => {
   const [programs, setPrograms] = useState([]);
@@ -65,27 +67,7 @@ const Proker = () => {
             <p className="mb-3 font-bold text-xs text-sky-500">
               {program.kompartemen}
             </p>
-            <a
-              href="#"
-              className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Read more
-              <svg
-                className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 10"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M1 5h12m0 0L9 1m4 4L9 9"
-                />
-              </svg>
-            </a>
+            <Modal />
           </div>
         ))}
       </div>
