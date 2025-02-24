7f01e9128c8e01feaa74df65958a1f6ed08130d9
syaafiudinm <altafpasallo12@gmail.com>
2025-02-25 00:17:19 +0800

    fix(blog card) : resize every card for image

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -60,36 +60,39 @@ export default function Blog() {
       <Navbar />
       <div className="mt-32 mx-4">
         <h1>OKIF Terkini</h1>
-        {blog.map((blog) => (
-          <div
-            key={blog.id}
-            className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700"
-          >
-            <Image
-              className="rounded-t-lg"
-              src={blog.image_url}
-              alt=""
-              width={100}
-              height={100}
-            />
-            <div className="p-5">
-              <a href="#">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                  {blog.title}
-                </h5>
-              </a>
-              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                {blog.content}
-              </p>
-              <a
-                href="#"
-                className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >
-                {blog.kompartemen}
-              </a>
+        <div className="flex flex-wrap gap-4 justify-center">
+          {blog.map((blog) => (
+            <div
+              key={blog.id}
+              className="w-80 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700"
+            >
+              <div className="relative w-full h-40">
+                <Image
+                  className="rounded-t-lg object-cover"
+                  src={blog.image_url}
+                  alt={blog.title}
+                  fill
+                />
+              </div>
+              <div className="p-5">
+                <a href="#">
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    {blog.title}
+                  </h5>
+                </a>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                  {blog.content}
+                </p>
+                <a
+                  href="#"
+                  className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  {blog.kompartemen}
+                </a>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
       <Footer />
     </>
